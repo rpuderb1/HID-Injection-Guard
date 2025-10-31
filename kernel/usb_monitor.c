8dfb18186082e35806623e9fcfcd2ea56339b6cb
Rylan P <rpuderb1@students.towson.edu>
2025-10-31 18:38:05 -0400

    Add USB notifier registration to kernel module
    - Register USB notifier callback for device events
    - Detect USB device connection and disconnection
    - Log events to kernel log via printk
    - Properly cleanup notifier on module unload

--- kernel/usb_monitor.c
+++ kernel/usb_monitor.c
@@ -2,6 +2,7 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/usb.h>
 
 // Module metadata
 MODULE_LICENSE("GPL");
@@ -10,15 +11,41 @@ MODULE_DESCRIPTION("USB HID Device Monitor");
 MODULE_VERSION("1.0");
 
 
+// Called whenever a USB device is added or removed
+static int usb_notify_callback(struct notifier_block *self, unsigned long action, void *dev) {
+    switch (action) {
+    case USB_DEVICE_ADD:
+        printk(KERN_INFO "usb_monitor: USB device connected\n");
+        break;
+    case USB_DEVICE_REMOVE:
+        printk(KERN_INFO "usb_monitor: USB device disconnected\n");
+        break;
+    }
+    return NOTIFY_OK;
+}
+
+// Links callback to the USB subsystem
+static struct notifier_block usb_notify = {
+    .notifier_call = usb_notify_callback,
+};
+
 // This function will be called when the module is loaded (insmod)
 static int __init usb_monitor_init(void) {
     printk(KERN_INFO "usb_monitor: Module loaded!\n");
+
+    // Register notifier to receive USB events
+    usb_register_notify(&usb_notify);
+    printk(KERN_INFO "usb_monitor: USB notifier registered\n");
+
     return 0;
 }
 
-
 // This function will be called when the module is unloaded (rmmod)
 static void __exit usb_monitor_exit(void) {
+    // Unregister USB notifier
+    usb_unregister_notify(&usb_notify);
+    printk(KERN_INFO "usb_monitor: USB notifier unregistered\n");
+
     printk(KERN_INFO "usb_monitor: Module unloaded!\n");
 }
 
